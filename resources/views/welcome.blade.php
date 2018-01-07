cbc3ab072e107f6b4f677e65acf7aeed4a09ed26
Matt Stauffer <matt@tighten.co>
2018-01-07 14:41:37 -0500

    Basic layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,38 +15,19 @@
 </head>
 <body class="bg-brand-lightest font-sans font-normal">
     <div class="flex flex-col">
-        @if(Route::has('login'))
-            <div class="absolute pin-t pin-r mt-4 mr-4">
-                @auth
-                    <a href="{{ url('/home') }}" class="no-underline hover:underline text-sm font-normal text-brand-dark uppercase">Home</a>
-                @else
-                    <a href="{{ route('login') }}" class="no-underline hover:underline text-sm font-normal text-brand-dark uppercase pr-6">Login</a>
-                    <a href="{{ route('register') }}" class="no-underline hover:underline text-sm font-normal text-brand-dark uppercase">Register</a>
-                @endauth
-            </div>
-        @endif
-
         <div class="min-h-screen flex items-center justify-center">
             <div class="flex flex-col justify-around h-full">
                 <div>
                     <h1 class="text-grey-darker text-center font-hairline tracking-wide text-7xl mb-6">
-                        {{ config('app.name', 'Laravel') }}
+                        <img src="logo-pad.png" alt="Stauffers On Science">
                     </h1>
+                    <h2>Subscribe:</h2>
                     <ul class="list-reset">
                         <li class="inline pr-8">
-                            <a href="https://laravel.com/docs" class="no-underline hover:underline text-sm font-normal text-brand-dark uppercase" title="Documentation">Documentation</a>
+                            <a href="#">Facebook Messenger</a>
                         </li>
                         <li class="inline pr-8">
-                            <a href="https://laracasts.com" class="no-underline hover:underline text-sm font-normal text-brand-dark uppercase" title="Laracasts">Laracasts</a>
-                        </li>
-                        <li class="inline pr-8">
-                            <a href="https://laravel-news.com" class="no-underline hover:underline text-sm font-normal text-brand-dark uppercase" title="News">News</a>
-                        </li>
-                        <li class="inline pr-8">
-                            <a href="https://forge.laravel.com" class="no-underline hover:underline text-sm font-normal text-brand-dark uppercase" title="Forge">Forge</a>
-                        </li>
-                        <li class="inline pr-8">
-                            <a href="https://github.com/laravel/laravel" class="no-underline hover:underline text-sm font-normal text-brand-dark uppercase" title="GitHub">GitHub</a>
+                            <a href="#">SMS (text message)</a>
                         </li>
                     </ul>
                 </div>
