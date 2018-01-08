0a9db5301d853dbe8ea6f9654950337efbea0334
Matt Stauffer <matt@tighten.co>
2018-01-07 21:02:02 -0500

    Style front page buttons and upgrade logo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,15 +19,15 @@
             <div class="flex flex-col justify-around h-full">
                 <div>
                     <h1 class="text-grey-darker text-center font-hairline tracking-wide text-7xl mb-6">
-                        <img src="logo-pad.png" alt="Stauffers On Science">
+                        <img src="/logo.svg" alt="Stauffers On Science">
                     </h1>
-                    <h2>Subscribe:</h2>
-                    <ul class="list-reset">
-                        <li class="inline pr-8">
-                            <a href="#">Facebook Messenger</a>
+                    <h2>Subscribe to new episodes:</h2>
+                    <ul class="list-reset my-4">
+                        <li class="inline">
+                            <a class="mt-6 sm:mt-0 mx-auto sm:mx-2 max-w-xs rounded-full text-center leading-none font-semibold inline-block px-8 py-3 border-2 border-teal bg-teal text-white hover:border-teal-light hover:bg-teal-light no-underline" href="/facebook/subscribe">Facebook Messenger</a>
                         </li>
-                        <li class="inline pr-8">
-                            <a href="#">SMS (text message)</a>
+                        <li class="inline">
+                            <a class="mt-6 sm:mt-0 mx-auto sm:mx-2 max-w-xs rounded-full text-center leading-none font-semibold inline-block px-8 py-3 border-2 border-teal bg-teal text-white hover:border-teal-light hover:bg-teal-light no-underline" href="/sms/subscribe">SMS (Text Message)</a>
                         </li>
                     </ul>
                 </div>
