3b5db4cce8a68d8d5c494f376497fc4139f4af86
Matt Stauffer <matt@tighten.co>
2018-01-13 00:16:08 -0500

    Add fb subscribe example

--- resources/views/facebook/instructions.blade.php
+++ resources/views/facebook/instructions.blade.php
@@ -7,5 +7,9 @@
         </a>
     </h1>
     <h2>Subscribe via Facebook:</h2>
-    <p>Click <a href="https://www.facebook.com/messages/t/stauffersonscience">here</a> and send us the message "Subscribe"</p>
+
+    <p class="mt-2">Click <a href="https://www.facebook.com/messages/t/stauffersonscience">here</a> and send us the message <code>subscribe</code>.</p>
+
+    <p class="mt-4 mb-1">Example:</p>
+    <a href="https://www.facebook.com/messages/t/stauffersonscience"><img src="/fb-subscribe-example.png" alt="How to subscribe via Facebook Messenger" class="max-w-sm"></a>
 @endsection
