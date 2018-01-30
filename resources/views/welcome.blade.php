84218fed415ce1868a73f59eb25f83e8a11f2bc7
Matt Stauffer <matt@tighten.co>
2018-01-30 12:48:32 -0500

    Better button margins?

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,17 +6,17 @@
     </h1>
     <h2>Subscribe to new episodes:</h2>
     <ul class="list-reset my-4">
-        <li class="inline">
-            <a class="mt-6 sm:mt-0 mx-auto sm:mx-2 max-w-xs rounded-full text-center leading-none font-semibold inline-block px-8 py-3 border-2 border-teal bg-teal text-white hover:border-teal-light hover:bg-teal-light no-underline" href="/facebook/subscribe">Facebook Messenger</a>
+        <li class="inline-block mb-6">
+            <a class="mx-auto sm:mx-2 max-w-xs rounded-full text-center leading-none font-semibold inline-block px-8 py-3 border-2 border-teal bg-teal text-white hover:border-teal-light hover:bg-teal-light no-underline" href="/facebook/subscribe">Facebook Messenger</a>
         </li>
-        <li class="inline">
-            <a class="mt-6 sm:mt-0 mx-auto sm:mx-2 max-w-xs rounded-full text-center leading-none font-semibold inline-block px-8 py-3 border-2 border-teal bg-teal text-white hover:border-teal-light hover:bg-teal-light no-underline" href="/sms/subscribe">SMS (Text Message)</a>
+        <li class="inline-block mb-6">
+            <a class="mx-auto sm:mx-2 max-w-xs rounded-full text-center leading-none font-semibold inline-block px-8 py-3 border-2 border-teal bg-teal text-white hover:border-teal-light hover:bg-teal-light no-underline" href="/sms/subscribe">SMS (Text Message)</a>
         </li>
-       <li class="inline">
-            <a class="mt-6 sm:mt-0 mx-auto sm:mx-2 max-w-xs rounded-full text-center leading-none font-semibold inline-block px-8 py-3 border-2 border-teal bg-teal text-white hover:border-teal-light hover:bg-teal-light no-underline" href="https://itunes.apple.com/us/podcast/stauffers-on-science/id1330250282?mt=2">iTunes</a>
+       <li class="inline-block mb-6">
+            <a class= mx-auto sm:mx-2 max-w-xs rounded-full text-center leading-none font-semibold inline-block px-8 py-3 border-2 border-teal bg-teal text-white hover:border-teal-light hover:bg-teal-light no-underline" href="https://itunes.apple.com/us/podcast/stauffers-on-science/id1330250282?mt=2">iTunes</a>
         </li>
-       <li class="inline">
-            <a class="mt-6 sm:mt-0 mx-auto sm:mx-2 max-w-xs rounded-full text-center leading-none font-semibold inline-block px-8 py-3 border-2 border-teal bg-teal text-white hover:border-teal-light hover:bg-teal-light no-underline" href="https://rss.simplecast.com/podcasts/4062/rss">RSS</a>
+       <li class="inline-block mb-6">
+            <a class= mx-auto sm:mx-2 max-w-xs rounded-full text-center leading-none font-semibold inline-block px-8 py-3 border-2 border-teal bg-teal text-white hover:border-teal-light hover:bg-teal-light no-underline" href="https://rss.simplecast.com/podcasts/4062/rss">RSS</a>
         </li>
     </ul>
 @endsection
