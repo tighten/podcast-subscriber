a6e942e0fd801d66a41ec5840692574f3e3609ba
Matt Stauffer <matt@tighten.co>
2018-01-30 12:49:13 -0500

    Stupid typos

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,10 +13,10 @@
             <a class="mx-auto sm:mx-2 max-w-xs rounded-full text-center leading-none font-semibold inline-block px-8 py-3 border-2 border-teal bg-teal text-white hover:border-teal-light hover:bg-teal-light no-underline" href="/sms/subscribe">SMS (Text Message)</a>
         </li>
        <li class="inline-block mb-6">
-            <a class= mx-auto sm:mx-2 max-w-xs rounded-full text-center leading-none font-semibold inline-block px-8 py-3 border-2 border-teal bg-teal text-white hover:border-teal-light hover:bg-teal-light no-underline" href="https://itunes.apple.com/us/podcast/stauffers-on-science/id1330250282?mt=2">iTunes</a>
+            <a class="mx-auto sm:mx-2 max-w-xs rounded-full text-center leading-none font-semibold inline-block px-8 py-3 border-2 border-teal bg-teal text-white hover:border-teal-light hover:bg-teal-light no-underline" href="https://itunes.apple.com/us/podcast/stauffers-on-science/id1330250282?mt=2">iTunes</a>
         </li>
        <li class="inline-block mb-6">
-            <a class= mx-auto sm:mx-2 max-w-xs rounded-full text-center leading-none font-semibold inline-block px-8 py-3 border-2 border-teal bg-teal text-white hover:border-teal-light hover:bg-teal-light no-underline" href="https://rss.simplecast.com/podcasts/4062/rss">RSS</a>
+            <a class="mx-auto sm:mx-2 max-w-xs rounded-full text-center leading-none font-semibold inline-block px-8 py-3 border-2 border-teal bg-teal text-white hover:border-teal-light hover:bg-teal-light no-underline" href="https://rss.simplecast.com/podcasts/4062/rss">RSS</a>
         </li>
     </ul>
 @endsection
